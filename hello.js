aeadb4c404ab3889486a571dcc6f1389cc67864c
Ken Bima Satria Gandasasmita <kenbima73@gmail.com>
2023-09-03 22:14:13 +0700

    new js files

---
+++ hello.js
@@ -0,0 +1,8 @@
+let nilaiA = 10;
+let nilaiB = 20;
+
+if (nilaiA> nilaiB){
+    console.log("berhasil")
+} else {
+    console.log("gagal")
+}
--- material.cpp
+++ material.cpp
@@ -3,18 +3,9 @@
 using namespace std;
 
 int main() {
-    int permen = 2000,
-    chiki = 4000,
-    biskuit = 6000,
-    x,
-    y,
-    n;
-    cout<<"masukkan jumlah permen:"<<endl;
-    cin>>x;
-    cout<<"masukkan jumlah chiki:"<<endl;
-    cin>>y;
-    cout<<"masukkan jumlah biskuit:"<<endl;
-    cin>>n;
-    
-    cout<<(permen*x)+(chiki*y)+(biskuit*n)<<endl;
+   float a= 10;
+
+   a*=12;
+
+   cout<<a<<endl;
 }
